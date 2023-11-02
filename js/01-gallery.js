703726d5ad050f0311a8e71139c72f16b733d07b
Maxym <103531640+MfD0@users.noreply.github.com>
2023-11-02 16:36:14 +0200

    1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+function createGalleryMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  }).join('');
+}
+
+function openModal(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const source = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+
+  instance.show();
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener('click', openModal);
